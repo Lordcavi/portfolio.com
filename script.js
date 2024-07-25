ff0d2562fe40fc49f3c9e89ce4aff269d3e5ea07
Akam Philip <akamisraelphilip@gmail.com>
2024-07-25 16:40:32 +0100

    adjusted the .active class to obey click scroll

--- script.js
+++ script.js
@@ -14,6 +14,19 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+// Active State
+document.addEventListener('DOMContentLoaded', function() {
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+      navLinks.forEach(link => link.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
+});
+
+
 
 const bar = document.getElementById("bar");
 const close = document.getElementById("close");
